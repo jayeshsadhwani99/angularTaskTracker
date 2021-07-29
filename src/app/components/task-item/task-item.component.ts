11bf40487eec3f30fa9c404bb22f59f95e580d35
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-07-29 17:18:15 +0530

    First commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
+
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task : Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+}
