8a6f1104af24009dd49c6e8b5eba8706dca9a932
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-07-29 18:16:04 +0530

    Toggle button and Add form

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 import { Observable } from 'rxjs';
 
 const httpOptions = {
